f708dc2fc0d6ead7acec7b8de23bda48feaf1f1d
MaximOG <maxim.mgoncharov2001@gmail.com>
2025-10-28 16:24:21 +0300

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -8,3 +8,13 @@ module.exports = { login };
 function checkCredentials(username, password) {  // опечатка в названии
     return username && password;
 }
+
+console.log('Debug: auth module loaded');  // отладочный код
+
+function login(username, password) {
+    if (checkCredentals(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
+}
+
