75b1cc0a66e0f9dd06dcc99e1c97c43dcf2d4f06
MaximOG <maxim.mgoncharov2001@gmail.com>
2025-10-28 16:25:13 +0300

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -5,6 +5,10 @@ function validateEmail(email) {
 
 module.exports = { validateEmail };
 
+
 function sanitizeInput(input) {
     return input.replace(/[<>]/g, '');  // защита от XSS
+}
+function validatePassword(password) {
+    return password.length >= 8;
 }
